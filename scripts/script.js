2e2cf1b5a4f590af7de3d246ba42bafdb574a9dc
pleiterson <pleiterson@gmail.com>
2020-11-30 18:48:50 -0300

    conclusao de toda a mecanica do jogo

--- scripts/script.js
+++ scripts/script.js
@@ -47,6 +47,13 @@ function startGame() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :(');
+        }
+    }
+
     createBG();
     createSnake();
     createFood();
