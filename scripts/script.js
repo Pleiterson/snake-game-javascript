04d88b9cc90863737e2839e10be73e0005941e4f
pleiterson <pleiterson@gmail.com>
2020-11-30 17:38:21 -0300

    criado a cobrinha na tela

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,23 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
+let snake = []; // criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG();
+function createBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0,0,16 * box, 16 * box); // desenha o retângulo usando x e y e a largura e altura setadas
+}
+
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+createBG();
+createSnake();
